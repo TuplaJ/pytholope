9d2be326125170324c079c57d26b57268f624670
TuplaJ <jeffrey.jackson@myy.haaga-helia.fi>
2015-09-09 12:44:34 +0300

    First commit

---
+++ lec2-5.py
@@ -0,0 +1,11 @@
+#example from lec 2.5 takes input and computes value
+
+x = 3
+x= x*x
+print(x)
+
+#prompt user and take input
+y = float(raw_input('Enter a number: '))
+#compute and print output
+print(y*y)
+
